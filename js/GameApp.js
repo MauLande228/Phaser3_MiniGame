2fcbee85326ad81ff56bc20c60b67100bbec31ea
Mauricio Landeros <jairo.landeros2885@alumnos.udg.mx>
2022-02-13 17:25:09 -0600

    Added platforms

--- js/GameApp.js
+++ js/GameApp.js
@@ -33,6 +33,12 @@ function preload()
 function create()
 {
     this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+    platforms.create(400, 568, 'platform').setScale(2).refreshBody();
+    platforms.create(600, 400, 'platform');
+    platforms.create(50, 250, 'platform');
+    platforms.create(750, 220, 'platform');
 }
 
 function update()
